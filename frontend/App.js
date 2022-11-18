2154da336da2fa57f4d2abe1ed5a9df82449cf2a
akshat <akshat.tandon@tum.de>
2022-11-19 00:51:23 +0100

    added routing

--- frontend/App.js
+++ frontend/App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import HomeComponent from "./components/HomeComponent";
+import LoginComponent from "./components/LoginComponent";
+import RegisterComponent from "./components/RegisterComponent";
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeComponent} />
+        <Stack.Screen name="Login" component={LoginComponent} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ frontend/components/RegisterComponent.js
@@ -0,0 +1,10 @@
+import {View} from "react-native"
+
+function RegisterComponent(props) {
+    return (
+        <View>
+        </View>    
+    );
+}
+
+export default RegisterComponent;
