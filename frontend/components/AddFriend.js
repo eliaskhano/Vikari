34809dc453e184c6d1f180c20e96c9a298e6b1b1
Ersjan Këri <ge87cin@mytum.de>
2022-11-20 04:32:43 +0100

    a

--- frontend/components/AddFriend.js
+++ frontend/components/AddFriend.js
@@ -1,90 +1,109 @@
 import React, { useMemo, useState, useEffect } from "react";
-import { SelectList } from "react-native-dropdown-select-list";
+import { SelectList } from 'react-native-dropdown-select-list'
 
-import { StyleSheet, View, Text, Button, Alert } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+} from "react-native";
 
+import { useGetUserListQuery } from "../services/mainApi";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
 import domain from "../domain";
-import { useNavigation } from '@react-navigation/native';
+
+
 
 function AddFriend(props) {
-  const navigation = useNavigation();     
   const getId = async () => {
-    return await AsyncStorage.getItem("token");
-  };
+    return await AsyncStorage.getItem("token")
+  }
 
   const [formattedOptions, setFormattedOptions] = useState([]);
-  const [targetId, selectTarget] = useState(null);
+  const [targetId, selectTarget] = useState(null)
+  const [following, setFollowing] = useState(Boolean)
+
 
   useEffect(() => {
     const call = async () => {
-      const user_id = await getId();
-      console.log(user_id);
+        const user_id = await getId();
+        console.log(user_id)
+    
+        await axios.get(`${domain}/users-api/list/${user_id}/`)
+        .then(res => {
+            setFormattedOptions(res.data)
+        })
+    }
+    call()
+  }, [])
 
-      await axios.get(`${domain}/users-api/list/${user_id}/`).then((res) => {
-        setFormattedOptions(res.data);
-      });
-    };
-    call();
-  }, []);
 
   const uploadFriend = async () => {
-    const token = await AsyncStorage.getItem("token");
+    const token = await AsyncStorage.getItem("token")
+  
 
     const context = {
-      target_id: targetId,
-    };
-
-    await axios
-      .post(`${domain}/api/friend/add/${token}/`, context)
-      .then((res) => {
-        Alert.alert("Successful :)", "Friend added!", [
+       target_id : targetId,
+    }
+   
+    await axios.post(`${domain}/api/friend/add/${token}/`, context)
+    .then(res => {
+      Alert.alert(
+        "Successful :)",
+        "Friend added!",
+        [
           {
             text: "Cancel",
-            onPress: () => navigation.navigate("Home"),
-            style: "cancel",
+            onPress: () => navigation.navigate('Home'),
+            style: "cancel"
           },
-          { text: "OK", onPress: () => navigation.navigate("Home") },
-        ]);
-      })
-      .catch((e) => {
-        Alert.alert("Error 404", "Something went wrong :(", [
+          { text: "OK", onPress: () => navigation.navigate('Home') }
+        ]
+      );
+    })
+    .catch(e => {
+      Alert.alert(
+        "Error 404",
+        "Something went wrong :(",
+        [
           {
             text: "Cancel",
             onPress: () => console.log("Cancel Pressed"),
-            style: "cancel",
+            style: "cancel"
           },
-          { text: "OK", onPress: () => navigation.navigate("Home") },
-        ]);
-      });
-  };
+          { text: "OK", onPress: () => navigation.navigate('Home') }
+        ]
+      );
+    })
+    
+  }
 
   return (
-    <View style={styles.container}>
-      <SelectList
-        setSelected={(e) => selectTarget(e)}
-        fontFamily="Arial"
-        data={formattedOptions}
-        boxStyles={{ borderRadius: 0, backgroundColor: "white" }} //override default styles
-        dropdownStyles={{ backgroundColor: "white" }}
-      />
+    <View >
+    
+        <SelectList 
+          setSelected={e => selectTarget(e)} 
+          fontFamily='Arial'
+          data={formattedOptions}  
+          boxStyles={{borderRadius:0}} //override default styles
+        />
+        
 
-      <View style={styles.button}>
-        <Button color="#FFFFFF" onPress={uploadFriend} title="ADD FRIEND">
-        </Button>
-      </View>
+        <Text  onPress={uploadFriend}>SUBMIT</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+
   container: {
     flex: 1,
-    backgroundColor: "#f5ccff",
-    padding: 20,
-    paddingTop: 40,
+    //backgroundColor: "#7b27d8",
+    alignItems: "center",
+    //justifyContent: "center",
   },
+
+
 
   submitButton: {
     width: "100%",
@@ -96,15 +115,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#460b87",
   },
 
-  button: {
-    width: "100%",
-    borderRadius: 25,
-    height: 50,
-    alignItems: "center",
-    justifyContent: "center",
-    marginTop: 40,
-    backgroundColor: "#460b87",
-  },
+  
 });
 
 export default AddFriend;
